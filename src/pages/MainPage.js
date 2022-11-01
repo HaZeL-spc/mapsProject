a8ab4288b3e57f06935aedc122f398e0f1b2dd06
Kacper <kacper.8leszczynski8@gmail.com>
2022-11-01 22:50:33 +0100

    added animation to image in mainpage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,6 +4,8 @@ import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 import "../css/MainPage.css";
 import { Link } from "react-router-dom";
 import InputView from "../components/InputView";
+import world from "../images/world.png";
+import car from "../images/car.png";
 
 // setup
 const provider = new OpenStreetMapProvider();
@@ -14,7 +16,11 @@ const MainPage = ({ setTotalData, totalData }) => {
   return (
     <div className="main-page-container">
       <InputView setTotalData={setTotalData} totalData={totalData} />
-      <div className="car-world-image"></div>
+      <div className="main-page-images">
+        {/* <div className="car-world-image"></div> */}
+        <img src={car} className="car-image" />
+        <img src={world} className="world-image" />
+      </div>
     </div>
   );
 };
